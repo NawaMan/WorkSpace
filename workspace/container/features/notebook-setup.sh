d3f102b01800ecd890370ec386f56e63fc8b7e1d
NawaMan <nawa@nawaman.net>
2025-09-06 18:19:23 -0400

    FEATURE_DIR and extract python.

--- workspace/container/features/notebook-setup.sh
+++ workspace/container/features/notebook-setup.sh
@@ -9,30 +9,14 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-apt-get update
-apt-get install -y \
-    --no-install-recommends \
-    python3 \
-    python3-venv \
-    python3-dev \
-    build-essential
+FEATURE_DIR=${FEATURE_DIR:-.}
 
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-
-if [ ! -d "/opt/venv" ]; then 
-    python3 -m venv /opt/venv
-fi
-
-/opt/venv/bin/python -m ensurepip --upgrade
+${FEATURE_DIR}/python-setup.sh
 
 /opt/venv/bin/pip      \
     install            \
     --no-cache-dir     \
     --no-compile       \
-    pip==25.2          \
-    setuptools==80.9.0 \
-    wheel==0.44.0      \
     jupyter==1.1.1     \
     jupyterlab==4.2.5  \
     notebook==7.2.2    \
