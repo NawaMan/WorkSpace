07f0137cef939a736773d0b2fb90ac7d5822d148
NawaMan <nawa@nawaman.net>
2025-10-26 14:01:34 -0400

    Fix the simple test.

--- examples/python-example/example.sh
+++ examples/python-example/example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../../workspace.sh --variant container -- bash -lc '
+../../workspace.sh --variant container -- '
 python3 - <<PY
 import os, sys
 print("CWD in container:", os.getcwd())
@@ -8,4 +8,5 @@ print("Args:", sys.argv)
 for i in range(3):
     print("line", i)
 PY
-'
+' \
+# 2>/dev/null      # Uncomment to get only the output of the program
