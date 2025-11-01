fdb614036e2e04f369852b1d44eba11588adc780
NawaMan <nawa@nawaman.net>
2025-11-01 11:54:40 -0400

    Remove PowerShell support.

--- tests/basic/run-all-test.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-Get-ChildItem -Path . -Filter 'test0*.ps1' -File |
-    Sort-Object Name |
-    ForEach-Object {
-        Write-Host $_.Name
-        & $_.FullName
-        Write-Host
-    }
