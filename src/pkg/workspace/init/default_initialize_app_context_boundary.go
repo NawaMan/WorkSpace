5f613059bf577e0f074bbe0bc71d6b1d3208af1d
NawaMan <nawa@nawaman.net>
2026-01-05 17:46:49 -0500

    Make GetUID and GetGID more cross platform.

--- src/pkg/workspace/init/default_initialize_app_context_boundary.go
+++ src/pkg/workspace/init/default_initialize_app_context_boundary.go
@@ -3,9 +3,8 @@ package init
 import (
 	"fmt"
 	"os"
-	"os/exec"
+	"os/user"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/kelseyhightower/envconfig"
@@ -60,34 +59,26 @@ func (DefaultInitializeAppContextBoundary) GetCurrentPath() string {
 	return cwd
 }
 
-func (DefaultInitializeAppContextBoundary) GetHostGID() string {
-	if runtime.GOOS == "windows" {
-		// Windows doesn't have GIDs, return a default
-		return "1000"
-	}
-
-	// Use id -g command
-	cmd := exec.Command("id", "-g")
-	output, err := cmd.Output()
-	if err != nil {
-		return "1000" // fallback
-	}
-
-	return strings.TrimSpace(string(output))
-}
-
 func (DefaultInitializeAppContextBoundary) GetHostUID() string {
 	if runtime.GOOS == "windows" {
-		// Windows doesn't have UIDs, return a default
 		return "1000"
 	}
 
-	// Use id -u command
-	cmd := exec.Command("id", "-u")
-	output, err := cmd.Output()
+	u, err := user.Current()
 	if err != nil {
-		return "1000" // fallback
+		return "1000"
+	}
+	return u.Uid
+}
+
+func (DefaultInitializeAppContextBoundary) GetHostGID() string {
+	if runtime.GOOS == "windows" {
+		return "1000"
 	}
 
-	return strings.TrimSpace(string(output))
+	u, err := user.Current()
+	if err != nil {
+		return "1000"
+	}
+	return u.Gid
 }
