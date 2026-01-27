0b49972d8a116ee72b40bb48dc754bfcb13a5bca
NawaMan <nawa@nawaman.net>
2026-01-27 08:18:17 -0500

    Implement[38;2;187;187;187m [39mremove[38;2;187;187;187m [39mcommand[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39mbooth[38;2;187;187;187m [39mcontainers
    Phase[38;2;187;187;187m [39m[38;2;102;102;102m1.6[39m[38;2;187;187;187m [39mof[38;2;187;187;187m [39mBooth[38;2;187;187;187m [39mLifecycle[38;2;187;187;187m [39mimplementation:
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39mfull[38;2;187;187;187m [39mimplementation[38;2;187;187;187m [39mof[38;2;187;187;187m [39m'remove'[38;2;187;187;187m [39mcommand
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mParse[38;2;187;187;187m [39mflags:[38;2;187;187;187m [39m[38;2;102;102;102m--[39mname,[38;2;187;187;187m [39m[38;2;102;102;102m-[39mf[38;2;102;102;102m/[39m[38;2;102;102;102m--[39mforce,[38;2;187;187;187m [39m[38;2;102;102;102m--[39mverbose
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mFind[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39mby[38;2;187;187;187m [39mname[38;2;187;187;187m [39m(inferred[38;2;187;187;187m [39mfrom[38;2;187;187;187m [39mcurrent[38;2;187;187;187m [39mdir[38;2;187;187;187m [39m[38;2;170;34;255;01mor[39;00m[38;2;187;187;187m [39mexplicit)
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mVerify[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39m[38;2;170;34;255;01mis[39;00m[38;2;187;187;187m [39mbooth[38;2;102;102;102m-[39mmanaged
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mSupport[38;2;187;187;187m [39mmultiple[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39mremoval[38;2;187;187;187m [39m(positional[38;2;187;187;187m [39margs)
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mProtect[38;2;187;187;187m [39mrunning[38;2;187;187;187m [39mcontainers[38;2;187;187;187m [39munless[38;2;187;187;187m [39m[38;2;102;102;102m--[39mforce[38;2;187;187;187m [39m[38;2;170;34;255;01mis[39;00m[38;2;187;187;187m [39mused
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39m[38;2;102;102;102m--[39mhelp[38;2;187;187;187m [39mwith[38;2;187;187;187m [39musage[38;2;187;187;187m [39mexamples
    
    Usage:
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mremove[38;2;187;187;187m                  [39m#[38;2;187;187;187m [39mRemove[38;2;187;187;187m [39mbooth[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39mcurrent[38;2;187;187;187m [39mdir
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mremove[38;2;187;187;187m [39mmy[38;2;102;102;102m-[39mproject[38;2;187;187;187m       [39m#[38;2;187;187;187m [39mRemove[38;2;187;187;187m [39mby[38;2;187;187;187m [39mname
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mremove[38;2;187;187;187m [39mproj1[38;2;187;187;187m [39mproj2[38;2;187;187;187m      [39m#[38;2;187;187;187m [39mRemove[38;2;187;187;187m [39mmultiple
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mremove[38;2;187;187;187m [39m[38;2;102;102;102m--[39mforce[38;2;187;187;187m          [39m#[38;2;187;187;187m [39mForce[38;2;187;187;187m [39mremove[38;2;187;187;187m [39mrunning[38;2;187;187;187m [39mcontainer
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mrm[38;2;187;187;187m                      [39m#[38;2;187;187;187m [39mAlias[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39mremove
    
    Co[38;2;102;102;102m-[39mAuthored[38;2;102;102;102m-[39mBy:[38;2;187;187;187m [39mClaude[38;2;187;187;187m [39mOpus[38;2;187;187;187m [39m[38;2;102;102;102m4.5[39m[38;2;187;187;187m [39m<noreply@anthropic.com>

--- cli/src/cmd/coding-booth/remove.go
+++ cli/src/cmd/coding-booth/remove.go
@@ -7,12 +7,141 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/nawaman/codingbooth/src/pkg/booth"
+	"github.com/nawaman/codingbooth/src/pkg/docker"
 )
 
 // removeBooth removes a stopped booth container.
 // Usage: coding-booth remove [--name <name>] [--force] [container...]
 func removeBooth(version string) {
-	fmt.Fprintln(os.Stderr, "Error: 'remove' command not yet implemented")
-	fmt.Fprintln(os.Stderr, "This will remove a booth container.")
-	os.Exit(1)
+	var (
+		containerName string
+		force         bool
+		verbose       bool
+	)
+
+	args := os.Args[2:] // Skip "coding-booth remove"
+	var positionalArgs []string
+
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "--name":
+			if i+1 >= len(args) {
+				fmt.Fprintln(os.Stderr, "Error: --name requires a value")
+				os.Exit(1)
+			}
+			i++
+			containerName = args[i]
+		case "-f", "--force":
+			force = true
+		case "--verbose":
+			verbose = true
+		case "-h", "--help":
+			showRemoveHelp()
+			return
+		default:
+			if strings.HasPrefix(args[i], "-") {
+				fmt.Fprintf(os.Stderr, "Unknown option: %s\n", args[i])
+				fmt.Fprintln(os.Stderr, "Use 'coding-booth remove --help' for usage information")
+				os.Exit(1)
+			}
+			positionalArgs = append(positionalArgs, args[i])
+		}
+	}
+
+	flags := docker.DockerFlags{
+		Dryrun:  false,
+		Verbose: verbose,
+		Silent:  false,
+	}
+
+	// Build list of containers to remove
+	var containers []string
+
+	if containerName != "" {
+		containers = append(containers, containerName)
+	}
+	containers = append(containers, positionalArgs...)
+
+	// If no containers specified, infer from current directory
+	if len(containers) == 0 {
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error: Cannot determine current directory")
+			os.Exit(1)
+		}
+		containers = append(containers, sanitizeName(filepath.Base(cwd)))
+	}
+
+	// Remove each container
+	hasError := false
+	for _, name := range containers {
+		// Verify container exists and is managed by booth
+		inspect, err := docker.InspectContainer(name, flags)
+		if err != nil {
+			if strings.Contains(err.Error(), "No such") {
+				fmt.Fprintf(os.Stderr, "Error: No booth '%s' found.\n", name)
+				hasError = true
+				continue
+			}
+			fmt.Fprintf(os.Stderr, "Error inspecting container '%s': %v\n", name, err)
+			hasError = true
+			continue
+		}
+
+		if inspect.Labels[booth.LabelManaged] != "true" {
+			fmt.Fprintf(os.Stderr, "Error: Container '%s' is not managed by CodingBooth.\n", name)
+			hasError = true
+			continue
+		}
+
+		// Check if container is running
+		if inspect.State.Running && !force {
+			fmt.Fprintf(os.Stderr, "Error: Container '%s' is running. Use --force to remove it.\n", name)
+			hasError = true
+			continue
+		}
+
+		// Remove the container
+		fmt.Printf("Removing booth '%s'...\n", name)
+
+		err = docker.RemoveContainer(name, force, flags)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error removing container '%s': %v\n", name, err)
+			hasError = true
+			continue
+		}
+
+		fmt.Printf("Booth '%s' removed.\n", name)
+	}
+
+	if hasError {
+		os.Exit(1)
+	}
+}
+
+func showRemoveHelp() {
+	fmt.Println(`Usage: coding-booth remove [OPTIONS] [CONTAINER...]
+
+Remove stopped booth containers.
+
+OPTIONS:
+  --name <name>    Container name to remove
+  -f, --force      Force remove even if running
+  --verbose        Show verbose output
+  -h, --help       Show this help
+
+If no names are provided, the container name is inferred from
+the current directory name.
+
+Multiple containers can be removed at once by listing them.
+
+EXAMPLES:
+  coding-booth remove                  # Remove booth for current directory
+  coding-booth remove my-project       # Remove booth by name
+  coding-booth remove proj1 proj2      # Remove multiple booths
+  coding-booth remove --force          # Force remove even if running`)
 }
