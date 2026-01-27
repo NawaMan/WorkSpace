fbab0377ec5f5cada52f51224433a9b1fb24033b
NawaMan <nawa@nawaman.net>
2026-01-27 08:20:52 -0500

    Implement[38;2;187;187;187m [39mrestore[38;2;187;187;187m [39mcommand[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mloading
    Phase[38;2;187;187;187m [39m[38;2;102;102;102m2.4[39m[38;2;187;187;187m [39mof[38;2;187;187;187m [39mBooth[38;2;187;187;187m [39mLifecycle[38;2;187;187;187m [39mimplementation:
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39mfull[38;2;187;187;187m [39mimplementation[38;2;187;187;187m [39mof[38;2;187;187;187m [39m[38;2;187;68;68m'[39m[38;2;187;68;68mrestore[39m[38;2;187;68;68m'[39m[38;2;187;187;187m [39mcommand
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mParse[38;2;187;187;187m [39mflags:[38;2;187;187;187m [39m[38;2;102;102;102m-[39m[38;2;102;102;102m-[39mverbose
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAccept[38;2;187;187;187m [39mfile[38;2;187;187;187m [39mpath[38;2;187;187;187m [39m[38;2;170;34;255;01mas[39;00m[38;2;187;187;187m [39mpositional[38;2;187;187;187m [39margument
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAuto[38;2;102;102;102m-[39mdetect[38;2;187;187;187m [39mgzip[38;2;187;187;187m [39mcompression[38;2;187;187;187m [39m(by[38;2;187;187;187m [39mextension[38;2;187;187;187m [39m[38;2;170;34;255;01mor[39;00m[38;2;187;187;187m [39mmagic[38;2;187;187;187m [39mbytes)
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mSupport[38;2;187;187;187m [39mboth[38;2;187;187;187m [39muncompressed[38;2;187;187;187m [39m([38;2;102;102;102m.[39mtar)[38;2;187;187;187m [39m[38;2;170;34;255;01mand[39;00m[38;2;187;187;187m [39mcompressed[38;2;187;187;187m [39m([38;2;102;102;102m.[39mtar[38;2;102;102;102m.[39mgz,[38;2;187;187;187m [39m[38;2;102;102;102m.[39mtgz)[38;2;187;187;187m [39mfiles
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mDisplay[38;2;187;187;187m [39mloaded[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mname
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mShow[38;2;187;187;187m [39musage[38;2;187;187;187m [39minstructions[38;2;187;187;187m [39mafter[38;2;187;187;187m [39msuccessful[38;2;187;187;187m [39mrestore
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39m[38;2;102;102;102m-[39m[38;2;102;102;102m-[39mhelp[38;2;187;187;187m [39mwith[38;2;187;187;187m [39musage[38;2;187;187;187m [39mexamples
    
    Usage:
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mrestore[38;2;187;187;187m [39mmywork[38;2;102;102;102m.[39mtar
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mrestore[38;2;187;187;187m [39mmywork[38;2;102;102;102m.[39mtar[38;2;102;102;102m.[39mgz
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mrestore[38;2;187;187;187m [39m[38;2;102;102;102m~[39m[38;2;102;102;102m/[39mbackups[38;2;102;102;102m/[39mmywork[38;2;102;102;102m.[39mtar
    
    Co[38;2;102;102;102m-[39mAuthored[38;2;102;102;102m-[39mBy:[38;2;187;187;187m [39mClaude[38;2;187;187;187m [39mOpus[38;2;187;187;187m [39m[38;2;102;102;102m4.5[39m[38;2;187;187;187m [39m[38;2;102;102;102m<[39mnoreply@anthropic[38;2;102;102;102m.[39mcom[38;2;102;102;102m>[39m

--- cli/src/cmd/coding-booth/restore.go
+++ cli/src/cmd/coding-booth/restore.go
@@ -7,12 +7,105 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/nawaman/codingbooth/src/pkg/docker"
 )
 
 // restoreImage loads an image from a tar file.
 // Usage: coding-booth restore <file>
 func restoreImage(version string) {
-	fmt.Fprintln(os.Stderr, "Error: 'restore' command not yet implemented")
-	fmt.Fprintln(os.Stderr, "This will load a Docker image from a tar file.")
-	os.Exit(1)
+	var verbose bool
+
+	args := os.Args[2:] // Skip "coding-booth restore"
+	var positionalArgs []string
+
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "--verbose":
+			verbose = true
+		case "-h", "--help":
+			showRestoreHelp()
+			return
+		default:
+			if strings.HasPrefix(args[i], "-") {
+				fmt.Fprintf(os.Stderr, "Unknown option: %s\n", args[i])
+				fmt.Fprintln(os.Stderr, "Use 'coding-booth restore --help' for usage information")
+				os.Exit(1)
+			}
+			positionalArgs = append(positionalArgs, args[i])
+		}
+	}
+
+	// File path is required
+	if len(positionalArgs) == 0 {
+		fmt.Fprintln(os.Stderr, "Error: File path is required")
+		fmt.Fprintln(os.Stderr, "Example: coding-booth restore myimage.tar")
+		os.Exit(1)
+	}
+
+	filePath := positionalArgs[0]
+
+	// Check if file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "Error: File not found: %s\n", filePath)
+		os.Exit(1)
+	}
+
+	flags := docker.DockerFlags{
+		Dryrun:  false,
+		Verbose: verbose,
+		Silent:  false,
+	}
+
+	// Detect if file is compressed
+	isCompressed := docker.IsCompressedArchive(filePath)
+
+	fmt.Printf("Loading image from '%s'", filePath)
+	if isCompressed {
+		fmt.Print(" (compressed)")
+	}
+	fmt.Println("...")
+
+	var imageName string
+	var err error
+
+	if isCompressed {
+		imageName, err = docker.LoadImageCompressed(filePath, flags)
+	} else {
+		imageName, err = docker.LoadImage(filePath, flags)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading image: %v\n", err)
+		os.Exit(1)
+	}
+
+	if imageName != "" {
+		fmt.Printf("Successfully loaded image: %s\n", imageName)
+		fmt.Println()
+		fmt.Println("To use this image:")
+		fmt.Printf("  coding-booth run --image %s\n", imageName)
+	} else {
+		fmt.Println("Image loaded successfully.")
+		fmt.Println("Run 'docker images' to see the loaded image.")
+	}
+}
+
+func showRestoreHelp() {
+	fmt.Println(`Usage: coding-booth restore [OPTIONS] <FILE>
+
+Load a Docker image from a tar file.
+
+OPTIONS:
+  --verbose   Show verbose output
+  -h, --help  Show this help
+
+The command automatically detects if the file is gzip-compressed
+(.tar.gz, .tgz, or by checking magic bytes).
+
+EXAMPLES:
+  coding-booth restore mywork.tar
+  coding-booth restore mywork.tar.gz
+  coding-booth restore ~/backups/mywork.tar`)
 }
