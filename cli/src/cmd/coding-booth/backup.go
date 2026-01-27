5e0e5d642f657fe2810e5b3b101f0a47a3c0f58f
NawaMan <nawa@nawaman.net>
2026-01-27 08:20:41 -0500

    Implement[38;2;187;187;187m [39mbackup[38;2;187;187;187m [39mcommand[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39mimage[38;2;187;187;187m [39marchival
    Phase[38;2;187;187;187m [39m[38;2;102;102;102m2.3[39m[38;2;187;187;187m [39mof[38;2;187;187;187m [39mBooth[38;2;187;187;187m [39mLifecycle[38;2;187;187;187m [39mimplementation:
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39mfull[38;2;187;187;187m [39mimplementation[38;2;187;187;187m [39mof[38;2;187;187;187m [39m'backup'[38;2;187;187;187m [39mcommand
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mParse[38;2;187;187;187m [39mflags:[38;2;187;187;187m [39m[38;2;102;102;102m-[39mo[38;2;102;102;102m/[39m[38;2;102;102;102m--[39moutput[38;2;187;187;187m [39m(required),[38;2;187;187;187m [39m[38;2;102;102;102m-[39mz[38;2;102;102;102m/[39m[38;2;102;102;102m--[39mcompress,[38;2;187;187;187m [39m[38;2;102;102;102m--[39mverbose
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAccept[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mname[38;2;187;187;187m [39m[38;2;170;34;255;01mas[39;00m[38;2;187;187;187m [39mpositional[38;2;187;187;187m [39margument
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mSupport[38;2;187;187;187m [39muncompressed[38;2;187;187;187m [39m(.tar)[38;2;187;187;187m [39m[38;2;170;34;255;01mand[39;00m[38;2;187;187;187m [39mcompressed[38;2;187;187;187m [39m(.tar.gz)[38;2;187;187;187m [39moutput
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAuto[38;2;102;102;102m-[39mappend[38;2;187;187;187m [39m.gz[38;2;187;187;187m [39mextension[38;2;187;187;187m [39mwhen[38;2;187;187;187m [39m[38;2;102;102;102m--[39mcompress[38;2;187;187;187m [39m[38;2;170;34;255;01mis[39;00m[38;2;187;187;187m [39mused
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mDisplay[38;2;187;187;187m [39mfile[38;2;187;187;187m [39msize[38;2;187;187;187m [39mafter[38;2;187;187;187m [39msuccessful[38;2;187;187;187m [39mbackup
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mShow[38;2;187;187;187m [39mrestore[38;2;187;187;187m [39minstructions
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39m[38;2;102;102;102m--[39mhelp[38;2;187;187;187m [39mwith[38;2;187;187;187m [39musage[38;2;187;187;187m [39mexamples
    
    Usage:
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mbackup[38;2;187;187;187m [39mmywork:v1[38;2;187;187;187m [39m[38;2;102;102;102m-[39mo[38;2;187;187;187m [39mmywork.tar
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mbackup[38;2;187;187;187m [39mmywork:v1[38;2;187;187;187m [39m[38;2;102;102;102m-[39mo[38;2;187;187;187m [39mmywork.tar.gz[38;2;187;187;187m [39m[38;2;102;102;102m--[39mcompress
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mbackup[38;2;187;187;187m [39mmywork:v1[38;2;187;187;187m [39m[38;2;102;102;102m-[39mo[38;2;187;187;187m [39m[38;2;102;102;102m~[39m[38;2;102;102;102m/[39mbackups[38;2;102;102;102m/[39mmywork.tar
    
    Co[38;2;102;102;102m-[39mAuthored[38;2;102;102;102m-[39mBy:[38;2;187;187;187m [39mClaude[38;2;187;187;187m [39mOpus[38;2;187;187;187m [39m[38;2;102;102;102m4.5[39m[38;2;187;187;187m [39m<noreply@anthropic.com>

--- cli/src/cmd/coding-booth/backup.go
+++ cli/src/cmd/coding-booth/backup.go
@@ -7,12 +7,134 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/nawaman/codingbooth/src/pkg/docker"
 )
 
 // backupImage saves an image to a tar file.
 // Usage: coding-booth backup <image> --output <file> [--compress]
 func backupImage(version string) {
-	fmt.Fprintln(os.Stderr, "Error: 'backup' command not yet implemented")
-	fmt.Fprintln(os.Stderr, "This will save a Docker image to a tar file.")
-	os.Exit(1)
+	var (
+		output   string
+		compress bool
+		verbose  bool
+	)
+
+	args := os.Args[2:] // Skip "coding-booth backup"
+	var positionalArgs []string
+
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "-o", "--output":
+			if i+1 >= len(args) {
+				fmt.Fprintln(os.Stderr, "Error: --output requires a value")
+				os.Exit(1)
+			}
+			i++
+			output = args[i]
+		case "-z", "--compress":
+			compress = true
+		case "--verbose":
+			verbose = true
+		case "-h", "--help":
+			showBackupHelp()
+			return
+		default:
+			if strings.HasPrefix(args[i], "-") {
+				fmt.Fprintf(os.Stderr, "Unknown option: %s\n", args[i])
+				fmt.Fprintln(os.Stderr, "Use 'coding-booth backup --help' for usage information")
+				os.Exit(1)
+			}
+			positionalArgs = append(positionalArgs, args[i])
+		}
+	}
+
+	// Image name is required
+	if len(positionalArgs) == 0 {
+		fmt.Fprintln(os.Stderr, "Error: Image name is required")
+		fmt.Fprintln(os.Stderr, "Example: coding-booth backup myimage:v1 -o myimage.tar")
+		os.Exit(1)
+	}
+
+	imageName := positionalArgs[0]
+
+	// Output file is required
+	if output == "" {
+		fmt.Fprintln(os.Stderr, "Error: --output is required")
+		fmt.Fprintln(os.Stderr, "Example: coding-booth backup myimage:v1 -o myimage.tar")
+		os.Exit(1)
+	}
+
+	flags := docker.DockerFlags{
+		Dryrun:  false,
+		Verbose: verbose,
+		Silent:  false,
+	}
+
+	// Add .gz extension if compressing and not already present
+	if compress && !strings.HasSuffix(strings.ToLower(output), ".gz") && !strings.HasSuffix(strings.ToLower(output), ".tgz") {
+		output = output + ".gz"
+	}
+
+	// Backup the image
+	fmt.Printf("Saving image '%s' to '%s'", imageName, output)
+	if compress {
+		fmt.Print(" (compressed)")
+	}
+	fmt.Println("...")
+
+	var err error
+	if compress {
+		err = docker.SaveImageCompressed(imageName, output, flags)
+	} else {
+		err = docker.SaveImage(imageName, output, flags)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error saving image: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Get file size
+	fileInfo, _ := os.Stat(output)
+	var sizeStr string
+	if fileInfo != nil {
+		size := fileInfo.Size()
+		if size > 1024*1024*1024 {
+			sizeStr = fmt.Sprintf("%.2f GB", float64(size)/(1024*1024*1024))
+		} else if size > 1024*1024 {
+			sizeStr = fmt.Sprintf("%.2f MB", float64(size)/(1024*1024))
+		} else if size > 1024 {
+			sizeStr = fmt.Sprintf("%.2f KB", float64(size)/1024)
+		} else {
+			sizeStr = fmt.Sprintf("%d bytes", size)
+		}
+	}
+
+	fmt.Printf("Successfully saved to '%s'", output)
+	if sizeStr != "" {
+		fmt.Printf(" (%s)", sizeStr)
+	}
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("To restore this image:")
+	fmt.Printf("  coding-booth restore %s\n", output)
+}
+
+func showBackupHelp() {
+	fmt.Println(`Usage: coding-booth backup [OPTIONS] <IMAGE>
+
+Save a Docker image to a tar file for offline sharing or backup.
+
+OPTIONS:
+  -o, --output <file>  Output file path (required)
+  -z, --compress       Compress with gzip (.tar.gz)
+  --verbose            Show verbose output
+  -h, --help           Show this help
+
+EXAMPLES:
+  coding-booth backup mywork:v1 -o mywork.tar
+  coding-booth backup mywork:v1 -o mywork.tar.gz --compress
+  coding-booth backup mywork:v1 -o ~/backups/mywork.tar`)
 }
