bc7fefd094803f64ddbd29103a51d3468f48eccf
NawaMan <nawa@nawaman.net>
2026-01-27 08:20:31 -0500

    Implement[38;2;187;187;187m [39mpush[38;2;187;187;187m [39mcommand[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mregistry[38;2;187;187;187m [39mupload
    Phase[38;2;187;187;187m [39m[38;2;102;102;102m2.2[39m[38;2;187;187;187m [39mof[38;2;187;187;187m [39mBooth[38;2;187;187;187m [39mLifecycle[38;2;187;187;187m [39mimplementation:
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39mfull[38;2;187;187;187m [39mimplementation[38;2;187;187;187m [39mof[38;2;187;187;187m [39m[38;2;187;68;68m'[39m[38;2;187;68;68mpush[39m[38;2;187;68;68m'[39m[38;2;187;187;187m [39mcommand
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mParse[38;2;187;187;187m [39mflags:[38;2;187;187;187m [39m[38;2;102;102;102m-[39m[38;2;102;102;102m-[39mregistry,[38;2;187;187;187m [39m[38;2;102;102;102m-[39m[38;2;102;102;102m-[39mverbose
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAccept[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mname[38;2;187;187;187m [39m[38;2;170;34;255;01mas[39;00m[38;2;187;187;187m [39mpositional[38;2;187;187;187m [39margument
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAuto[38;2;102;102;102m-[39mtag[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mwith[38;2;187;187;187m [39mregistry[38;2;187;187;187m [39mprefix[38;2;187;187;187m [39m[38;2;170;34;255;01mif[39;00m[38;2;187;187;187m [39mspecified
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mPush[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mto[38;2;187;187;187m [39mDocker[38;2;187;187;187m [39mregistry
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mShow[38;2;187;187;187m [39musage[38;2;187;187;187m [39minstructions[38;2;187;187;187m [39mafter[38;2;187;187;187m [39msuccessful[38;2;187;187;187m [39mpush
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39m[38;2;102;102;102m-[39m[38;2;102;102;102m-[39mhelp[38;2;187;187;187m [39mwith[38;2;187;187;187m [39musage[38;2;187;187;187m [39mexamples
    
    Usage:
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mpush[38;2;187;187;187m [39mmywork:v1
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mpush[38;2;187;187;187m [39mmywork:v1[38;2;187;187;187m [39m[38;2;102;102;102m-[39m[38;2;102;102;102m-[39mregistry[38;2;187;187;187m [39mghcr[38;2;102;102;102m.[39mio[38;2;102;102;102m/[39mmyuser
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mpush[38;2;187;187;187m [39mghcr[38;2;102;102;102m.[39mio[38;2;102;102;102m/[39mmyuser[38;2;102;102;102m/[39mmywork:v1
    
    Co[38;2;102;102;102m-[39mAuthored[38;2;102;102;102m-[39mBy:[38;2;187;187;187m [39mClaude[38;2;187;187;187m [39mOpus[38;2;187;187;187m [39m[38;2;102;102;102m4.5[39m[38;2;187;187;187m [39m[38;2;102;102;102m<[39mnoreply@anthropic[38;2;102;102;102m.[39mcom[38;2;102;102;102m>[39m

--- cli/src/cmd/coding-booth/push.go
+++ cli/src/cmd/coding-booth/push.go
@@ -7,12 +7,107 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/nawaman/codingbooth/src/pkg/docker"
 )
 
 // pushImage pushes a committed image to a registry.
 // Usage: coding-booth push <image> [--registry <url>]
 func pushImage(version string) {
-	fmt.Fprintln(os.Stderr, "Error: 'push' command not yet implemented")
-	fmt.Fprintln(os.Stderr, "This will push a Docker image to a registry.")
-	os.Exit(1)
+	var (
+		registry string
+		verbose  bool
+	)
+
+	args := os.Args[2:] // Skip "coding-booth push"
+	var positionalArgs []string
+
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "--registry":
+			if i+1 >= len(args) {
+				fmt.Fprintln(os.Stderr, "Error: --registry requires a value")
+				os.Exit(1)
+			}
+			i++
+			registry = args[i]
+		case "--verbose":
+			verbose = true
+		case "-h", "--help":
+			showPushHelp()
+			return
+		default:
+			if strings.HasPrefix(args[i], "-") {
+				fmt.Fprintf(os.Stderr, "Unknown option: %s\n", args[i])
+				fmt.Fprintln(os.Stderr, "Use 'coding-booth push --help' for usage information")
+				os.Exit(1)
+			}
+			positionalArgs = append(positionalArgs, args[i])
+		}
+	}
+
+	// Image name is required
+	if len(positionalArgs) == 0 {
+		fmt.Fprintln(os.Stderr, "Error: Image name is required")
+		fmt.Fprintln(os.Stderr, "Example: coding-booth push myimage:v1")
+		os.Exit(1)
+	}
+
+	imageName := positionalArgs[0]
+
+	flags := docker.DockerFlags{
+		Dryrun:  false,
+		Verbose: verbose,
+		Silent:  false,
+	}
+
+	// If registry is specified, tag the image with the registry prefix
+	targetImage := imageName
+	if registry != "" {
+		// If the image doesn't already include the registry, prepend it
+		if !strings.Contains(imageName, "/") || !strings.Contains(strings.Split(imageName, "/")[0], ".") {
+			targetImage = registry + "/" + imageName
+		}
+
+		fmt.Printf("Tagging '%s' as '%s'...\n", imageName, targetImage)
+		err := docker.TagImage(imageName, targetImage, flags)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error tagging image: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Push the image
+	fmt.Printf("Pushing '%s'...\n", targetImage)
+
+	err := docker.PushImage(targetImage, flags)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error pushing image: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully pushed '%s'\n", targetImage)
+	fmt.Println()
+	fmt.Println("To use this image:")
+	fmt.Printf("  coding-booth run --image %s\n", targetImage)
+}
+
+func showPushHelp() {
+	fmt.Println(`Usage: coding-booth push [OPTIONS] <IMAGE>
+
+Push a Docker image to a container registry.
+
+OPTIONS:
+  --registry <url>  Registry URL (e.g., ghcr.io/username)
+  --verbose         Show verbose output
+  -h, --help        Show this help
+
+If --registry is specified and the image doesn't include a registry,
+the image will be tagged with the registry prefix before pushing.
+
+EXAMPLES:
+  coding-booth push mywork:v1
+  coding-booth push mywork:v1 --registry ghcr.io/myuser
+  coding-booth push ghcr.io/myuser/mywork:v1`)
 }
