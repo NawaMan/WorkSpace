e0e8535cba187939125389229fab2deeb5bbb649
NawaMan <nawa@nawaman.net>
2026-01-27 08:20:20 -0500

    Implement[38;2;187;187;187m [39mcommit[38;2;187;187;187m [39mcommand[38;2;187;187;187m [39m[38;2;170;34;255;01mfor[39;00m[38;2;187;187;187m [39msaving[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39mstate
    Phase[38;2;187;187;187m [39m[38;2;102;102;102m2.1[39m[38;2;187;187;187m [39mof[38;2;187;187;187m [39mBooth[38;2;187;187;187m [39mLifecycle[38;2;187;187;187m [39mimplementation:
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39mfull[38;2;187;187;187m [39mimplementation[38;2;187;187;187m [39mof[38;2;187;187;187m [39m'commit'[38;2;187;187;187m [39mcommand
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mParse[38;2;187;187;187m [39mflags:[38;2;187;187;187m [39m[38;2;102;102;102m--[39mname,[38;2;187;187;187m [39m[38;2;102;102;102m-[39mt[38;2;102;102;102m/[39m[38;2;102;102;102m--[39mtag[38;2;187;187;187m [39m(required),[38;2;187;187;187m [39m[38;2;102;102;102m-[39mm[38;2;102;102;102m/[39m[38;2;102;102;102m--[39mmessage,[38;2;187;187;187m [39m[38;2;102;102;102m--[39mverbose
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mFind[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39mby[38;2;187;187;187m [39mname[38;2;187;187;187m [39m(inferred[38;2;187;187;187m [39mfrom[38;2;187;187;187m [39mcurrent[38;2;187;187;187m [39mdir[38;2;187;187;187m [39m[38;2;170;34;255;01mor[39;00m[38;2;187;187;187m [39mexplicit)
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mVerify[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39m[38;2;170;34;255;01mis[39;00m[38;2;187;187;187m [39mbooth[38;2;102;102;102m-[39mmanaged
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mCreate[38;2;187;187;187m [39mDocker[38;2;187;187;187m [39mimage[38;2;187;187;187m [39mfrom[38;2;187;187;187m [39mcontainer[38;2;187;187;187m [39mstate
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mShow[38;2;187;187;187m [39mnext[38;2;187;187;187m [39msteps[38;2;187;187;187m [39mafter[38;2;187;187;187m [39msuccessful[38;2;187;187;187m [39mcommit
    [38;2;102;102;102m-[39m[38;2;187;187;187m [39mAdd[38;2;187;187;187m [39m[38;2;102;102;102m--[39mhelp[38;2;187;187;187m [39mwith[38;2;187;187;187m [39musage[38;2;187;187;187m [39mexamples
    
    Usage:
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mcommit[38;2;187;187;187m [39m[38;2;102;102;102m--[39mtag[38;2;187;187;187m [39mmywork:v1
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mcommit[38;2;187;187;187m [39m[38;2;102;102;102m--[39mtag[38;2;187;187;187m [39mmywork:v1[38;2;187;187;187m [39m[38;2;102;102;102m--[39mname[38;2;187;187;187m [39mmy[38;2;102;102;102m-[39mproject
    [38;2;187;187;187m  [39mcoding[38;2;102;102;102m-[39mbooth[38;2;187;187;187m [39mcommit[38;2;187;187;187m [39m[38;2;102;102;102m--[39mtag[38;2;187;187;187m [39mmywork:v1[38;2;187;187;187m [39m[38;2;102;102;102m-[39mm[38;2;187;187;187m [39m[38;2;187;68;68m"Added dependencies"[39m
    
    Co[38;2;102;102;102m-[39mAuthored[38;2;102;102;102m-[39mBy:[38;2;187;187;187m [39mClaude[38;2;187;187;187m [39mOpus[38;2;187;187;187m [39m[38;2;102;102;102m4.5[39m[38;2;187;187;187m [39m<noreply@anthropic.com>

--- cli/src/cmd/coding-booth/commit.go
+++ cli/src/cmd/coding-booth/commit.go
@@ -7,12 +7,144 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/nawaman/codingbooth/src/pkg/booth"
+	"github.com/nawaman/codingbooth/src/pkg/docker"
 )
 
 // commitBooth creates a Docker image from a container's current state.
 // Usage: coding-booth commit --tag <tag> [--name <name>] [--message <msg>]
 func commitBooth(version string) {
-	fmt.Fprintln(os.Stderr, "Error: 'commit' command not yet implemented")
-	fmt.Fprintln(os.Stderr, "This will save a booth container as a Docker image.")
-	os.Exit(1)
+	var (
+		containerName string
+		tag           string
+		message       string
+		verbose       bool
+	)
+
+	args := os.Args[2:] // Skip "coding-booth commit"
+	var positionalArgs []string
+
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
+		case "--name":
+			if i+1 >= len(args) {
+				fmt.Fprintln(os.Stderr, "Error: --name requires a value")
+				os.Exit(1)
+			}
+			i++
+			containerName = args[i]
+		case "-t", "--tag":
+			if i+1 >= len(args) {
+				fmt.Fprintln(os.Stderr, "Error: --tag requires a value")
+				os.Exit(1)
+			}
+			i++
+			tag = args[i]
+		case "-m", "--message":
+			if i+1 >= len(args) {
+				fmt.Fprintln(os.Stderr, "Error: --message requires a value")
+				os.Exit(1)
+			}
+			i++
+			message = args[i]
+		case "--verbose":
+			verbose = true
+		case "-h", "--help":
+			showCommitHelp()
+			return
+		default:
+			if strings.HasPrefix(args[i], "-") {
+				fmt.Fprintf(os.Stderr, "Unknown option: %s\n", args[i])
+				fmt.Fprintln(os.Stderr, "Use 'coding-booth commit --help' for usage information")
+				os.Exit(1)
+			}
+			positionalArgs = append(positionalArgs, args[i])
+		}
+	}
+
+	// Tag is required
+	if tag == "" {
+		fmt.Fprintln(os.Stderr, "Error: --tag is required")
+		fmt.Fprintln(os.Stderr, "Example: coding-booth commit --tag myimage:v1")
+		os.Exit(1)
+	}
+
+	flags := docker.DockerFlags{
+		Dryrun:  false,
+		Verbose: verbose,
+		Silent:  false,
+	}
+
+	// Determine which container to commit
+	if containerName == "" && len(positionalArgs) > 0 {
+		containerName = positionalArgs[0]
+	}
+
+	if containerName == "" {
+		// Try to infer from current directory
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error: Cannot determine current directory")
+			os.Exit(1)
+		}
+		containerName = sanitizeName(filepath.Base(cwd))
+	}
+
+	// Verify container exists and is managed by booth
+	inspect, err := docker.InspectContainer(containerName, flags)
+	if err != nil {
+		if strings.Contains(err.Error(), "No such") {
+			fmt.Fprintf(os.Stderr, "Error: No booth '%s' found.\n", containerName)
+			fmt.Fprintln(os.Stderr, "Use 'coding-booth list' to see available containers.")
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stderr, "Error inspecting container: %v\n", err)
+		os.Exit(1)
+	}
+
+	if inspect.Labels[booth.LabelManaged] != "true" {
+		fmt.Fprintf(os.Stderr, "Error: Container '%s' is not managed by CodingBooth.\n", containerName)
+		fmt.Fprintln(os.Stderr, "Use 'coding-booth list' to see booth-managed containers.")
+		os.Exit(1)
+	}
+
+	// Commit the container
+	fmt.Printf("Committing booth '%s' as '%s'...\n", containerName, tag)
+
+	err = docker.CommitContainer(containerName, tag, message, flags)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error committing container: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully created image '%s'\n", tag)
+	fmt.Println()
+	fmt.Println("Next steps:")
+	fmt.Printf("  • Push to registry:   coding-booth push %s\n", tag)
+	fmt.Printf("  • Save to file:       coding-booth backup %s -o <file>.tar\n", tag)
+	fmt.Printf("  • Run the image:      coding-booth run --image %s\n", tag)
+}
+
+func showCommitHelp() {
+	fmt.Println(`Usage: coding-booth commit [OPTIONS] [CONTAINER]
+
+Create a Docker image from a container's current state.
+
+OPTIONS:
+  --name <name>       Container name to commit
+  -t, --tag <tag>     Image tag to create (required)
+  -m, --message <msg> Commit message
+  --verbose           Show verbose output
+  -h, --help          Show this help
+
+If no container name is provided, it is inferred from the current directory.
+
+EXAMPLES:
+  coding-booth commit --tag mywork:v1
+  coding-booth commit --tag mywork:v1 --name my-project
+  coding-booth commit --tag mywork:v1 -m "Added dependencies"
+  coding-booth commit my-project --tag registry.example.com/mywork:v1`)
 }
